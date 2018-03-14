2ec631c828af75f5bf831c541d7422e17a5cd9b7
Aurèle <aurele.barriere@gmail.com>
2018-03-14 22:36:15 +0100

    added tosin's code to verify it

---
+++ btrees/verif_relation_mem.v
@@ -0,0 +1,12 @@
+(** * verif_btrees.v: Correctness proof of relation_mem.c *)
+
+Require Import VST.floyd.proofauto.
+Require Import VST.floyd.library.
+Require Import relation_mem.
+Instance CompSpecs : compspecs. make_compspecs prog. Defined.
+Definition Vprog : varspecs.  mk_varspecs prog. Defined.
+
+Require Import VST.msl.wand_frame.
+Require Import VST.msl.iter_sepcon.
+Require Import VST.floyd.reassoc_seq.
+Require Import VST.floyd.field_at_wand.
